9a0ca31619ba026692f7ad75c8915113db104f49
Naofumi Okamoto <nokamoto.engr@gmail.com>
2025-03-29 13:59:31 +0000

    Add Magefile for build automation

---
+++ magefiles/magefile.go
@@ -0,0 +1,34 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+var g0 = sh.RunCmd("go")
+
+var Default = Build
+
+func Build() error {
+	mg.SerialDeps(Fmt, Import, Tidy, Test)
+	return nil
+}
+
+func Fmt() error {
+	return g0("run", "mvdan.cc/gofumpt", "-w", ".")
+}
+
+func Import() error {
+	return g0("run", "golang.org/x/tools/cmd/goimports", "-w", ".")
+}
+
+func Tidy() error {
+	return g0("mod", "tidy")
+}
+
+func Test() error {
+	return g0("test", "./...")
+}
